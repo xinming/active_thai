694230d50a16ab9dafc8bdf0991c78e25bbb1800
xinming <zhaoxmmusic@msn.com>
2011-10-29 01:11:50 +0700

    made the pagination better

--- app/controllers/thai_words_controller.rb
+++ app/controllers/thai_words_controller.rb
@@ -1,6 +1,12 @@
 class ThaiWordsController < InheritedResources::Base
   respond_to :html, :xml, :json
-  has_scope :page, :default => 1
+  # has_scope :page, :default => 1
   has_scope :completed
   has_scope :to_be_completed
+  
+  def collection
+    end_of_association_chain.order("created_at DESC").page(params[:page] || 1)
+  end
+  
+  
 end
