1a62718c0bc36f03d135addb246cb095c3cc89cf
xinming <zhaoxmmusic@msn.com>
2011-11-01 20:30:50 +0700

    added params ?

--- app/controllers/thai_words_controller.rb
+++ app/controllers/thai_words_controller.rb
@@ -1,5 +1,7 @@
 class ThaiWordsController < InheritedResources::Base
   respond_to :html, :xml, :json
+  has_scope :to_be_completed
+  has_scope :completed
   def collection
     end_of_association_chain.order("created_at DESC").page(params[:page] || 1)
   end
