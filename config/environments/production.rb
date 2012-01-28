fb853f8b25848108bb7ab68c2354da413c10d9dc
Xinming Zhao <xinming@ohho.(none)>
2012-01-28 12:56:45 +0700

    just new words

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ ThaiAdmin::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -57,4 +57,7 @@ ThaiAdmin::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+#  config.action_controller.relative_url_root = "/thai"
+  # RAILS_RELATIVE_URL_ROOT="/thai"
+  ENV['RAILS_RELATIVE_URL_ROOT'] = '/foo'# 
 end
